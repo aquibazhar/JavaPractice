3402087cffd8a309ffa219d3af56bd94dd5b01c3
Aquib Azhar <aaquibazhar1802@gmail.com>
2022-01-20 16:11:11 +0530

    Added Sum.java

---
+++ Sum.java
@@ -0,0 +1,12 @@
+package LearningGit;
+
+public class Sum {
+    public static void main(Strng args[]) {
+        int a = 10;
+        int b = 18;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
