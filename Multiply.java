0f54df63ca0638d2ed40583d4819770905731ec0
Aquib Azhar <aaquibazhar1802@gmail.com>
2022-01-20 16:41:07 +0530

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+package LearningGit;
+
+public class Multiply {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 18;
+
+        System.out.println(a * b);
+    }
+}
